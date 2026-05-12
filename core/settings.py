d4bfbfc0ca65d77c8b814f0748eb995a427130cb
Kapil DEv <65942292+kdev257@users.noreply.github.com>
2026-05-12 17:18:19 +0530

    Restore Django settings with initial comments

--- core/settings.py
+++ core/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'True'
-SECRET_KEY = ''
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
